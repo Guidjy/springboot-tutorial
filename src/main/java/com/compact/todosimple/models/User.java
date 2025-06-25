f6ae7cf29d588977e607465b3fe8ea11ebb2c532
Guidjy <guippimentel@gmail.com>
2025-06-25 19:08:48 -0300

    add password encoder and create ProfileEnum

--- src/main/java/com/compact/todosimple/models/User.java
+++ src/main/java/com/compact/todosimple/models/User.java
@@ -2,6 +2,7 @@ package com.compact.todosimple.models;
 
 
 
+import com.compact.todosimple.models.enums.ProfileEnum;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
@@ -10,9 +11,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 // trata a classe como uma tabela do db
 @Entity
@@ -50,4 +50,20 @@ public class User {
     // não quer ler as tasks
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
+
+    // sempre que buscar um usuário do banco, busca os perfis juntos
+    @ElementCollection(fetch = FetchType.EAGER)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @CollectionTable(name = "user_profile")
+    @Column(name = "profile", nullable = false)
+    private Set<Integer> profiles = new HashSet<>();
+
+    // tem que ser feito se não o getter do lombok iria retornar uma lista de inteiros
+    public Set<ProfileEnum> getProfiles() {
+        return this.profiles.stream().map(x -> ProfileEnum.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addProfile(ProfileEnum profileEnum) {
+        this.profiles.add(profileEnum.getCode());
+    }
 }
--- src/main/java/com/compact/todosimple/services/UserService.java
+++ src/main/java/com/compact/todosimple/services/UserService.java
@@ -1,16 +1,23 @@
 package com.compact.todosimple.services;
 
 import com.compact.todosimple.models.User;
+import com.compact.todosimple.models.enums.ProfileEnum;
 import com.compact.todosimple.repositories.UserRepository;
 import com.compact.todosimple.services.exceptions.DataBindingViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired  // "meio que um construtor do service"
     private UserRepository userRepository;
@@ -26,6 +33,10 @@ public class UserService {
     public User create(User obj) {
         // garante que um novo usuário será criado
         obj.setId(null);
+        // salva a senha encriptada
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+        // transforma stream em lista de usuários de tipo 2 (USER)
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
 
         obj = this.userRepository.save(obj);
         return obj;
@@ -37,6 +48,7 @@ public class UserService {
         User newObj = findById(obj.getId());
 
         newObj.setPassword(obj.getPassword());
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
